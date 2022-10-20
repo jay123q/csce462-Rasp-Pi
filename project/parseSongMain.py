1dda6407384ac3211650c2efe23d8eef97e169ce
jay123q <106704608+jay123q@users.noreply.github.com>
2022-10-20 09:52:17 -0500

    Speedup SlowDown compelete
    speedup and slow down made in main, with boilerplate to show the feasibilty of assigning pins to the GPIO blocks

--- project/parseSongMain.py
+++ project/parseSongMain.py
@@ -16,6 +16,7 @@ filePathtoParsedSongs = '/Users/Joshua/Documents/github/PersonalGit/csce462-Rasp
 --------------------------------------------------------------------------------------------------------------------------
 '''
 
+from random import sample
 from playsound import playsound #pip install playsound==1.2.2
 import wave
 import numpy as np
@@ -31,66 +32,18 @@ def buttonSong(parsedSong = dict):
         playsound(parsedSong[i])
 
 
-def writeNewSongs(filePathtoParsedSongs, parsedSong, sampleRate):
+def writeNewSongs(filePathtoParsedSongs, parsedSong, sampleRate, lowPass, highPass, speedUp, slowDown):
     '''This is'''
-
-    print(parsedSong)
+    sampleRate = int( speedUp*sampleRate / slowDown )
 
     for i in range(len(parsedSong)):
 
         fileName = filePathtoParsedSongs + 'songPart'+str(i) + '.wav'
         scipy.wavfile.write(fileName,sampleRate,parsedSong[i])
-        ''' parsedSongNumpy = np.array(parsedSong[i])
-        parsedSongNumpy = parsedSongNumpy.astype('<U32')
-        headerAddPart = np.concatenate((header.astype('<U32'),parsedSongNumpy), axis=0)
-        headerAddPart = headerAddPart.astype('float32')
-        #headerAddPart = convert_bytearray_to_wav_ndarray(headerAddPart)
-        mem_buf = io.BytesIO( ) # we are converting to a mp3 then back to wav
-        mem_buf.name = (fileName)
-        sf.write( mem_buf, headerAddPart, fr, format='WAV' )
-        mem_buf.seek( 0 )
-        data, samplerate = sf.read(mem_buf)
-        #fileName = filePathtoParsedSongs + 'songPart'+str(counter) + '.wav'
-        p_data = sf.write(fileName, data , samplerate, subtype='PCM_16')'''
 
+def songPicker():
+    '''logic to up down and select song'''
 
-def parseSongOLDFAILED(songPath):
-    '''THIS IS OUTDATED might make a bin file for it later'''
-    wf = wave.open(songPath, 'rb')
-    #nchan = wf.getparams().nframes
-    #swidth = wf.getparams().sampwidth
-    fr = wf.getparams().framerate
-    bin_data = wf.readframes(wf.getparams().nframes)
-
-    bd = []
-    for my_byte in bin_data:
-      bd.append(f'{my_byte:0>8b}')
-
-    countHeaderIndex = 0
-    parsedHeader = np.array([])
-
-    for i in range(len(bd)):
-        if(countHeaderIndex > 43):
-            break
-        else:
-            countHeaderIndex+=1
-            parsedHeader = np.append(parsedHeader,bd[i])
-
-    countPosRemove = 0
-
-    parsingIndex = (len(bd) - 44 ) /8 # 8 buttons
-    parsingKeyCount=0
-    dictKeyIndex = 0
-
-    partSongs = {0: [],1 : [],2:[],3:[],4:[],5:[],6:[],7:[]}
-    for i in range(44,len(bd)):
-            if(parsingIndex < parsingKeyCount):
-                dictKeyIndex +=1
-                parsingKeyCount = 0
-            partSongs[dictKeyIndex].append( bd[i])
-            parsingKeyCount+=1
-
-    return partSongs,fr,parsedHeader
 
 def parseSongWav(songPath):
     """implementation of the scipy waveform and parsing it """
@@ -108,17 +61,64 @@ def parseSongWav(songPath):
 
 def main():
 
-    userInput = 'drumSounds1'
-    # here add a polling system to change the song type & get user input
-    # the buttons a - d ( first 4 will instantly change the song type )
-    songPath = '/Users/Joshua/Documents/github/PersonalGit/csce462-Rasp-Pi/project/songs/' + userInput + '.wav'
+    backToLast = 0
     filePathtoParsedSongs = '/Users/Joshua/Documents/github/PersonalGit/csce462-Rasp-Pi/project/parsedSong/'
     #parsedFN = "16bitsnare"
-    parsedWavs, sampleRate = parseSongWav(songPath)
-    writeNewSongs(filePathtoParsedSongs,parsedWavs, sampleRate)
-    #organizedDict, fr,parsedHeader = parseSongWav(songPath)
-    #writeNewSongs(filePathtoParsedSongs,organizedDict, fr, parsedHeader)
 
+
+    # directory control logic here
+    userInput = 'drumSounds1'
+    songPath = '/Users/Joshua/Documents/github/PersonalGit/csce462-Rasp-Pi/project/songs/' + userInput + '.wav'
+    
+
+
+    parsedWavs, sampleRate = parseSongWav(songPath)
+
+    highFilter = 1
+    lowFilter = 1   
+    highFilterPin = 1
+    lowFilterPin = 0
+
+       # test here by changing these values for speedup slowdown 
+    speedUpRate = 1
+    slowDownRate = 1
+    speedUpPin = 1
+    slowDownPin  = 0
+    
+
+
+
+    # replace this block with gpio pin detection
+    if (lowFilterPin):
+        # dumby values change later 
+        # trigger something to filter?
+        lowFilter = 2
+    if (highFilterPin):
+        # some change to apply a filter
+        # trigger something to filter?
+        highFilter = 2
+    
+    # if input detected here change list back to old function
+    if(backToLast):
+        print("shifting back to song select ")
+
+    # replace this block with gpio pin detection
+    if (speedUpPin):
+        speedUpRate = 2
+    if (slowDownPin):
+        # this might need logic to better parse the song
+        slowDownRate = 2
+     
+        
+    if(backToLast):
+        print("shifting back to low/hight")
+
+    
+    writeNewSongs(filePathtoParsedSongs,parsedWavs, sampleRate,lowFilter,highFilter,speedUpRate,slowDownRate)
+    highFilter = 0
+    lowFilter = 0
+    speedUp = 1
+    slowDown = 1
 
 if (__name__ == "__main__"):
     main()
