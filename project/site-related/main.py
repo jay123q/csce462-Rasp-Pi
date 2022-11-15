fee60545471e329f6076776763b7e3aebda90d56
MainLaptop <40152651+Quivun@users.noreply.github.com>
2022-11-15 16:16:53 -0600

    dependency research 4 real time data streaming of backend api to front end

--- project/site-related/main.py
+++ project/site-related/main.py
@@ -1,51 +1,23 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
+import json
+import asyncio
+import jinja2
+from fastapi import FastAPI,Request,WebSocket
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
+with open('measurements.json', 'r') as file:
+    measurements = iter(json.loads(file.read()))
 
 @app.get("/")
-async def get():
-    return HTMLResponse(html)
-
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        await asyncio.sleep(0.1)
+        payload = next(measurements)
+        await websocket.send_json(payload)
