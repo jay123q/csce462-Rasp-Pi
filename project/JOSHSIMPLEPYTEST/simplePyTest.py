260fbae8914fa4b0c7298e621e9b08d12058e991
jay123q <106704608+jay123q@users.noreply.github.com>
2022-11-29 13:31:50 -0600

    pushing tinker test

---
+++ project/JOSHSIMPLEPYTEST/simplePyTest.py
@@ -0,0 +1,25 @@
+# %%
+import tkinter  as tk
+
+def defaultTest():
+
+
+    window = tk.Tk()
+    label = tk.Label(
+        text="Hello, Tkinter",
+        fg="white",
+        bg="black",
+        width=10,
+        height=10
+    )
+    label.pack()
+
+    window.mainloop()
+
+
+    
+
+
+
+defaultTest()
+# %%
