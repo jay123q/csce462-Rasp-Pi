6f96fec3856cb5355198f40c165af8e624936001
MainLaptop <40152651+Quivun@users.noreply.github.com>
2022-11-30 05:21:17 -0600

    Update audioFunctions.py

--- project/audioFunctions.py
+++ project/audioFunctions.py
@@ -92,7 +92,7 @@ def parseSongWav(pathInput):
     if len(beats) > 1:
         bpm = np.median(60./np.diff(beats))
 
-    return box.append(data), sampleRate, bpm
+    return np.append(box,data), sampleRate, bpm
 
 # Divides audio into 8 segments
 def writeSong(pathInput, pathOutput, fName, passState = 0, speedMultiplier = 1.0, fractional = 1.0):
